5ad4bbb3636dae12ebf87b2c0ed10664a5999968
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 13:32:48 +0400

    Payment Method

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3054caaeba8f6f17dfd1');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '8f320be1097161756574');
