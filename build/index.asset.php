577eec5cd82092df9c948937e12755fd165fe1f4
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 18:51:18 +0400

    price format

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd938e8f23d556026246b');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '69d4d87cd65f1f0ec966');
