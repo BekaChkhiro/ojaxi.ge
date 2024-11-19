b1c274a432b3e62220c9d405d0772077a03405ce
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 17:01:11 +0400

    old changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '9c1833150298e65e01ef');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3054caaeba8f6f17dfd1');
--- react-template.php
+++ react-template.php
@@ -2,44 +2,29 @@
 /**
  * Template Name: React Template
  */
-
-// Redirect to default template if this is a checkout or order-pay page
-if (is_checkout() || is_order_pay_page()) {
-    return include(WC()->plugin_path() . '/templates/checkout/form-checkout.php');
-}
-
 global $post;
 $product = wc_get_product($post->ID);
 
-// Get product data
+// მივიღოთ პროდუქტის მონაცემები
 $product_data = array(
     'productId' => $post->ID,
     'productSlug' => $post->post_name
 );
-
-get_header();
 ?>
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php if ($product): ?>
         <title><?php echo esc_html($product->get_name()); ?> - <?php bloginfo('name'); ?></title>
     <?php endif; ?>
-    <?php wp_head(); ?>
     <script>
+        // გადავცეთ პროდუქტის მონაცემები React აპლიკაციას
         window.initialProductData = <?php echo json_encode($product_data); ?>;
     </script>
 </head>
-<body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    
-    <div id="root"></div>
-    
-    <?php wp_footer(); ?>
-</body>
-</html>
+<?php  wp_head();
+    get_header();
+?>
+
+<div id="root"></div>
 
 <?php get_footer(); ?>
