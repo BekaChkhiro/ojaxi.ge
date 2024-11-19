476d235285240092c305657182b17f4e0b41fd0d
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 15:08:56 +0400

    Chnages for Fondy

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '29de67b0e0bf5b2ae6fb');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '13dea1021635eee5a5cc');
