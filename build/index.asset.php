22ff6cb6fd5ac32d517372e7c0e99adb42bac4a2
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 18:12:10 +0400

    price changed

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '75d28de5565074ed6425');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '4bfcb19f2ebd92e3759c');
