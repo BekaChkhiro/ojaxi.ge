e49ac08d7928d9fdbad8cb1b56806b8064e76873
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 16:43:18 +0400

    woocommerce error handling

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '7c35fb7f9454f37e43e4');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '4590e65dddeffc8fdd0d');
