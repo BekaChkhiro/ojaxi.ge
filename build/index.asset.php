ac49ebb7c676777c130d0a62e76dfe552822545b
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 15:34:52 +0400

    fondy standart payments

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'bacb94842471ed00b541');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd0953bd3d026ebc13585');
