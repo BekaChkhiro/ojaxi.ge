d173bf64b69d0772367fc98ddc1a823b50b9fdef
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-19 18:53:35 +0400

    right side bar price format

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '69d4d87cd65f1f0ec966');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '4ac01ac3f436b651c13b');
