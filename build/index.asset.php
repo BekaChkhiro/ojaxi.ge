2cc41193288b551176734054c68140714e1bc18a
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-21 15:17:09 +0400

    checkout mobile

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'f503bf5a5fbf82766816');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3a6b7b3bbc27e18f5ecc');
