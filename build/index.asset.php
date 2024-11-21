59bb0bf94a012d9b09002bbf63a64bed02f9b617
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-21 11:10:12 +0400

    MObile Navbar Styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '5b1e29350aad15ae5c5c');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '8b21950edbadf110786a');
