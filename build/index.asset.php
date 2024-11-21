1230bad0afc61aa0da82275fb3e051abac3bcd33
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-21 16:25:05 +0400

    confetti

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'e42c1529fa010d8061c5');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3f5fbecdfa67f881935a');
