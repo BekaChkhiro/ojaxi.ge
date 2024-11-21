e35c84c2c3a0863deedfe951e10ee7118bca8f4a
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-21 11:04:32 +0400

    Mobile NavBar changed

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '4ac01ac3f436b651c13b');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '5b1e29350aad15ae5c5c');
