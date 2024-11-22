6483d02d4f3e0200b3529935a83883a28cf92d36
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-22 10:39:37 +0400

    Checkout srcoll

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '1648a16f3cd43d1d808b');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '3a6b595adb0cccf0dc96');
