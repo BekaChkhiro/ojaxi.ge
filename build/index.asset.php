0da11f7dd4197d39530a1fd2e8a4c6f871e885a2
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-27 19:23:51 +0400

    safari changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'bb96035bd6799cba3551');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'c3918279f43b5b97f76b');
