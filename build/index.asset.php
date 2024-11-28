c696c42172a4254729a03db50cd7106c287e4c0f
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-28 11:18:42 +0400

    old product list

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '65709779302d87d50718');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'e3b07c9641afe3f4ede1');
