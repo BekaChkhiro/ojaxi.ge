2d9f60f31a6be3b65b36be0680a510f1a0e3f09d
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-28 12:43:39 +0400

    old functions

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '5bfcda6027dc2cb6dd83');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '391ca5d5a854ccc514e2');
