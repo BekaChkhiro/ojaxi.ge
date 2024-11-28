f535f7e6e38369b2a99e018af22fa3c615ca4de1
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-11-28 12:56:20 +0400

    cart functiuons

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '92f619dfacae2e40a7ad');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '147b6666e49b5a94b846');
