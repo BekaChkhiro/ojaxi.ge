4eed88a5e7933c4118fcc62832e9620804aac805
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-12-02 17:58:06 +0400

    change mobile and tablet

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '0d01902bf32fad3fbe77');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '91e6dd1026b1c414b251');
